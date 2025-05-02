af30981803bacd233e62ce4138ce642b2d879538
Waledahmed <Waledahmed416@gmail.com>
2025-05-03 00:38:08 +0200

    resend_otp LocalCameraWithChatPage.dart------join meeting api_service.dart--------all api

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -2,7 +2,7 @@
 const appId = "a1aa2d92946342348223ca4e573082af";
 // Fill in the temporary token generated from Agora Console
 const token =
-    "007eJxTYJgpqebu/4jr/I/w24dYF+VcPWq2tTS26PbRFdFmTFHMoZEKDImGiYlGKZZGliZmxiZGxiYWRkbGyYkmqabmxgYWRolpr5cKZTQEMjIci3rOzMgAgSA+K0N5Yk5qCgMDACoaHwo=";
+    "007eJxTYAgsLYrb++lzslrx5o63O43Co+efyRZr/XD5c9CZgHV2y78rMCQaJiYapVgaWZqYGZsYGZtYGBkZJyeapJqaGxtYGCWmbXISzWgIZGSwP7GamZEBAkF8VobyxJzUFAYGAAiJIV0=";
 // Fill in the channel name you used to generate the token
 const channel = "waled";
-const String apiBase = "https://f9e4-156-210-156-140.ngrok-free.app";
+const String apiBase = "https://02a4-197-54-186-8.ngrok-free.app";
--- lib/network/apiService.dart
+++ lib/network/apiService.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class Apiservice {
   static const String baseUrl =
-      "https://7267-197-132-247-156.ngrok-free.app/api/";
+      "https://cb72-197-132-247-156.ngrok-free.app/api/";
 
   final dio = Dio();
   Future<Response> post(String endpoint, Map<String, dynamic> data) async {
